598e3ec61a95112c849ea23314734a5843c86846
prestond <david.preston@fujitsu.com>
2021-02-17 00:14:53 +0000

    fixes

--- navigation_handler.py
+++ navigation_handler.py
@@ -8,13 +8,13 @@ class NavigationHandler(CommandHandler):
 
     def go(self, game, location_name):
         location = game.get_location()
-        if(location_name):  
-            location_item = location.get_inventory_item(location_name)
+        if(location_name):              
             if(location.has_inventory_item(location_name)):
                 utils.print_message("I dont know how to get to '{}' - Try: go <place>".format(
                     colour.red(location_name)
                 ))
                 return
+            location_item = location.get_inventory_item(location_name)
             if(not game.locations.is_valid_location_name(location_name)):
                 if("attributes" in location_item._config and "room" in location_item._config["attributes"]):
                     utils.print_message("Config Error: room '{}' not in config.".format(
